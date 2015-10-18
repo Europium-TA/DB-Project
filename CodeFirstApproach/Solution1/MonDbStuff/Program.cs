b67c9cb4636a3e8ac68a33c58ea4e8d8529234a5
PavelNPavlov <pavelnapavlov@gmail.com>
2015-10-18 10:41:50 +0300

    changes

---
+++ CodeFirstApproach/Solution1/MonDbStuff/Program.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonDbStuff
+{
+    class Program
+    {
+        static void Main()
+        {
+            MongoClient client = new MongoClient("mongodb://localhost");
+
+            var dataBase = client.GetDatabase("test");
+        }
+    }
+}
